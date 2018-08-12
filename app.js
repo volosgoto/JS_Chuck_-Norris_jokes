40962e7f80abd6930469d4465ae1496ce22ba544
andrey <volosgoto@yahoo.com>
2018-08-12 15:17:03 +0300

    insert data into UI

--- app.js
+++ app.js
@@ -4,24 +4,28 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(e) {
     // let number = document.getElementById('number').value;
     let number = document.querySelector('input[type="number"]').value;
-
     let xhr = new XMLHttpRequest();
-    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
     xhr.onload = function() {
         if (this.status === 200) {
-            let response = this.responseText;
-            console.log(response);
 
+            let response = JSON.parse(this.responseText);
+            let output = '';
+
+            if (response.type === 'success') {
+                response.value.forEach(joke => {
+                    output += `<li>${joke.joke}</li>`
+                });
+            } else {
+                output += '<li>Something wrong!</li>'
+            }
+            document.querySelector('.jokes').innerHTML = output;
         }
     }
-
-
-
 
     xhr.send();
 
     e.preventDefault();
-
 }
