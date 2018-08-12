4f7cfb782cfa5bae461bdeabf419adb97d58ea31
andrey <volosgoto@yahoo.com>
2018-08-12 15:02:03 +0300

    get data from API

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 function getJokes(e) {
     // let number = document.getElementById('number').value;
     let number = document.querySelector('input[type="number"]').value;
-    console.log(number);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
 
 
+    xhr.onload = function() {
+        if (this.status === 200) {
+            let response = this.responseText;
+            console.log(response);
+
+        }
+    }
+
+
+
+
+    xhr.send();
 
     e.preventDefault();
 
